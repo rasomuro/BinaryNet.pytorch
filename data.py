3118db521a269ebf17e6038d68817b241c749540
Mauro Brunato <mauro.brunato.tn@gmail.com>
2024-08-05 11:25:00 +0200

    Converting to newer Pytorch API

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import os
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
-_DATASETS_MAIN_PATH = '/home/Datasets'
+_DATASETS_MAIN_PATH = '.'
 _dataset_path = {
     'cifar10': os.path.join(_DATASETS_MAIN_PATH, 'CIFAR10'),
     'cifar100': os.path.join(_DATASETS_MAIN_PATH, 'CIFAR100'),
